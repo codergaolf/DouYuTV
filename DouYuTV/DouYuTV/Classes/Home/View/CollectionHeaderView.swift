f7d9c73b53234b2510be5b5c014bd0e1d44d3817
高立发 <GLF@ArisGLFdeMacBook-Pro.local>
2016-11-14 21:45:56 +0800

    自定义CollectionPrettyCell

---
+++ DouYuTV/DouYuTV/Classes/Home/View/CollectionHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionHeaderView.swift
+//  DouYuTV
+//
+//  Created by 高立发 on 2016/11/14.
+//  Copyright © 2016年 GG. All rights reserved.
+//
+
+import UIKit
+
+class CollectionHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ DouYuTV/DouYuTV/Classes/Main/View/CollectionNormalCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionNormalCell.swift
+//  DouYuTV
+//
+//  Created by 高立发 on 2016/11/14.
+//  Copyright © 2016年 GG. All rights reserved.
+//
+
+import UIKit
+
+class CollectionNormalCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
