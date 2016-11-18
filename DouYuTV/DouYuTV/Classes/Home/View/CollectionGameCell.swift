b8297906031b3a6b8755a318db9f1796ddfaaae4
高立发 <GLF@ArisGLFdeMacBook-Pro.local>
2016-11-19 00:00:09 +0800

    推荐游戏,数据展示

---
+++ DouYuTV/DouYuTV/Classes/Home/View/CollectionGameCell.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionGameCell.swift
+//  DouYuTV
+//
+//  Created by 高立发 on 2016/11/18.
+//  Copyright © 2016年 GG. All rights reserved.
+//
+
+import UIKit
+
+class CollectionGameCell: UICollectionViewCell {
+
+    // MARK:- 控件属性
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    // MARK:- 定义模型属性
+    var group : AnchorGroup? {
+        didSet {
+            titleLabel.text = group?.tag_name
+            let iconURL = URL(string: group?.icon_url ?? "")
+            iconImageView.kf.setImage(with: iconURL, placeholder: UIImage(named: "home_more_btn"))
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- DouYuTV/DouYuTV/Classes/Home/View/RecommendGameView.swift
+++ DouYuTV/DouYuTV/Classes/Home/View/RecommendGameView.swift
@@ -9,8 +9,25 @@
 import UIKit
 
 private let kGameCellID = "kGameCellID"
+private let kEdgeInsetMargin : CGFloat = 15
 
 class RecommendGameView: UIView {
+    
+    // MARK:- 定义数据的属性
+    var groups : [AnchorGroup]? {
+        didSet {
+            //1,移除前两组数据
+            groups?.removeFirst()
+            groups?.removeFirst()
+            
+            //2,添加更多组
+            let moreGroup = AnchorGroup()
+            moreGroup.tag_name = "更多"
+            groups?.append(moreGroup)
+            
+            collectionView.reloadData()
+        }
+    }
     
     // MARK:- 控件属性
     @IBOutlet weak var collectionView: UICollectionView!
@@ -23,7 +40,10 @@ class RecommendGameView: UIView {
         autoresizingMask = UIViewAutoresizing()
         
         //注册cell
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "kGameCellID")
+        collectionView.register(UINib(nibName: "CollectionGameCell", bundle: nil), forCellWithReuseIdentifier: kGameCellID)
+        
+        //给collectionView添加内编剧
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: kEdgeInsetMargin, bottom: 0, right: kEdgeInsetMargin)
     }
 }
 
@@ -37,13 +57,14 @@ extension RecommendGameView {
 // MARK:- 遵守UICollectionView的数据源协议
 extension RecommendGameView : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return groups?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kGameCellID, for: indexPath)
         
-        cell.backgroundColor = indexPath.item % 2 == 0 ? UIColor.red : UIColor.blue
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kGameCellID, for: indexPath) as! CollectionGameCell
+        
+        cell.group = groups![indexPath.item]
         
         return cell
     }
